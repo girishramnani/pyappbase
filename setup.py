3042e889ac81ba50548c7e9d624468c9ead7e4fd
girishramnani <girishramnani95@gmail.com>
2016-05-19 18:19:04 +0530

    travis setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="pyappbase",
+      version="0.0.1",
+      author="girish ramnani",
+      packages=find_packages(),
+      zip_safe=False,
+      license="MIT")
