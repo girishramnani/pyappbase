c6ac7c147056b3b9255e136b988d22eee4d4e08c
girishramnani <girishramnani95@gmail.com>
2016-06-04 03:07:44 +0530

    removing useless method

--- pyappbase/utils.py
+++ pyappbase/utils.py
@@ -22,36 +22,6 @@ def conditional_coroutine(func):
     return wrapper
 
 
-def map_object(data):
-    """
-
-    maps the input object for update and other operations having payload
-    of type {
-                type : ---
-                id : ---
-                body : new-content
-            }
-          To
-            {
-                type : ---
-                id : ---
-                body : { doc : new-content }
-            }
-    which is accepted by the appbase server
-
-
-    :param data:
-    :return:
-    """
-
-    body = data["body"]
-
-    data["body"] = {
-        "doc": body
-    }
-
-    return data
-
 
 def make_url(url,data):
     return "".join([url, "/", data["type"], "/", data["id"]])
