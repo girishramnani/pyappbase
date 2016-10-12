9baf70324cf2b5efe510df69fcce92db36abe945
girishramnani <girishramnani95@gmail.com>
2016-10-12 15:35:07 +0530

    removed useless code

--- pyappbase/utils.py
+++ pyappbase/utils.py
@@ -1,28 +1,5 @@
 import asyncio
 
 
-
-
-def conditional_coroutine(func):
-    from pyappbase.handler import AsyncHandler
-
-    def wrapper(*args,**kwargs):
-
-        try:
-            self = args[0]
-
-            for attribute in self.__dict__:
-                if isinstance(getattr(self, attribute), AsyncHandler):
-                    return asyncio.coroutine(func)
-
-        except IndexError:
-            raise IndexError("the function wrapped is not a method")
-
-        return func
-
-    return wrapper
-
-
-
 def make_url(url,data):
     return "".join([url, "/", data["type"], "/", data["id"]])
